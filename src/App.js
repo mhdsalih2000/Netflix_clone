bc45263f31ab8ea1e8d3e55f4d892e007366e0b8
mhd swalih <MuhammedSwalih034@gmail.com>
2023-12-26 18:18:33 +0530

    static netflix clone done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './Component/Navbar/NavBar';
+import Banner from './Component/Banner/Banner.js';
+import RawPost from './Component/RawPost/RawPost.js';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+    <NavBar/>
+    <Banner/>
+    <RawPost/>
+   
+     
     </div>
   );
 }
---
+++ src/Component/Navbar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='nav_bar'>
+        <img className='Netflix_Logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt='netflix-logo' />
+        <img className='Avatar' src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png' alt='avatar-logo' />
+
+    </div>
+  )
+}
+
+export default NavBar
